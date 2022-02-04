8d0f5320285118e19cc8bcd8e9fe1e4e91d71185
ProPuke <ProPuke@31i73.com>
2022-02-04 07:14:03 +0000

    improved: set all properties to readonly

--- yare.io.d.ts
+++ yare.io.d.ts
@@ -2,69 +2,69 @@ type Shape = 'circles'|'squares'|'triangles';
 type Vector = [number,number];
 
 declare interface Entity {
-	id: string;
-	position: Vector;
-	energy: number;
-	energy_capacity: number;
+	readonly id: string;
+	readonly position: Vector;
+	readonly energy: number;
+	readonly energy_capacity: number;
 }
 
 declare interface Structure extends Entity {
-	size: number;
-	collision_radius: number;
-	structure_type: 'base'|'outpost'|'star';
+	readonly size: number;
+	readonly collision_radius: number;
+	readonly structure_type: 'base'|'outpost'|'star';
 }
 
 declare interface Base extends Structure {
-	structure_type: 'base';
-	player_id: string;
-	shape: Shape;
-	sight: {
-		friends_beamable:string[];
-		enemies_beamable:string[];
-		friends:string[];
-		enemies:string[];
-		structures:string[];
+	readonly structure_type: 'base';
+	readonly player_id: string;
+	readonly shape: Shape;
+	readonly sight: {
+		readonly friends_beamable:string[];
+		readonly enemies_beamable:string[];
+		readonly friends:string[];
+		readonly enemies:string[];
+		readonly structures:string[];
 	};
-	hp: 0|1;
-	color: string; //"rgba(128,140,255,1)" etc
-	current_spirit_cost: number
+	readonly hp: 0|1;
+	readonly color: string; //"rgba(128,140,255,1)" etc
+	readonly current_spirit_cost: number
 }
 
 declare interface Outpost extends Structure {
-	structure_type: 'outpost';
-	control: string;
-	range: number;
-	last_energized: string;
-	sight: {
-		enemies: string[]
+	readonly structure_type: 'outpost';
+	readonly control: string;
+	readonly range: number;
+	readonly last_energized: string;
+	readonly sight: {
+		readonly enemies: string[]
 	};
 }
 
 declare interface Star extends Structure {
-	structure_type: 'star';
-	last_energized: string;
-	active_in: number; //?
-	active_out: number; //?
+	readonly structure_type: 'star';
+	readonly last_energized: string;
+	readonly active_in: number; //?
+	readonly active_out: number; //?
 }
 
 declare interface Spirit extends Entity {
-	player_id: string;
-	shape: Shape;
-	size: number;
-	last_energized: string;
-	color: string; //"rgba(128,140,255,1)" etc.
-	mark: string;
-	sight: {
-		friends_beamable: string[];
-		enemies_beamable: string[];
-		friends: string[];
-		enemies: string[];
-		structures: string[];
+	readonly player_id: string;
+	readonly shape: Shape;
+	readonly size: number;
+	readonly last_energized: string;
+	readonly color: string; //"rgba(128,140,255,1)" etc.
+	readonly mark: string;
+	readonly sight: {
+		readonly friends_beamable: string[];
+		readonly enemies_beamable: string[];
+		readonly friends: string[];
+		readonly enemies: string[];
+		readonly structures: string[];
 	};
-	merged: string[];
-	qcollisions: string[]; //?
-	hp: 0|1;
-	move_speed: number; //might be a multiple of 20? starts at 1
+	readonly merged: string[];
+	readonly qcollisions: string[]; //?
+	readonly hp: 0|1;
+	readonly move_speed: number; //might be a multiple of 20? starts at 1
 
 	set_mark(mark:string):void;
 	shout(message:string):void;
@@ -84,7 +84,7 @@ interface Graphics {
 	rect(topLeft:Vector, bottomRight:Vector):void;
 }
 
-declare let memory: Record<string, any>;
+declare const memory: Record<string, any>;
 declare const spirits: Record<string, Spirit>;
 declare const my_spirits: Spirit[];
 declare const stars: Record<string, Star>;
